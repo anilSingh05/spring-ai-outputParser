cb61ad182b3addbb378b497a8c3cceb6b640e330
Anil Singh <anil05.me@gmail.com>
2025-01-07 21:30:22 -0600

    Added  ListOutputConverter

--- src/main/java/spring_ai_outputParser/SongsController.java
+++ src/main/java/spring_ai_outputParser/SongsController.java
@@ -4,11 +4,13 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,8 +23,27 @@ public class SongsController {
         this.chatClient = chatClient.build();
     }
 
-    @GetMapping("/songs")
-    public String songOutputParser(@RequestParam(value = "artist", defaultValue = "Kishor Kumar") String artist){
+    @GetMapping("/songs-by-artist")
+    public List<String> getSongsByArtistInFormattedOutPut(@RequestParam(value = "artist", defaultValue = "Kumar Shanu") String artist){
+
+        var message  = """
+                Please give  me a list of top 10 songs for the artist {artist} . If you  don't know the answer, just say
+                I don't know!
+                {format}
+                """;
+
+        ListOutputConverter outputConverter = new ListOutputConverter(new DefaultConversionService());
+
+        PromptTemplate promptTemplate =  new PromptTemplate(message, Map.of("artist", artist, "format", outputConverter.getFormat()));
+        Prompt prompt = promptTemplate.create();
+        ChatResponse chatResponse = chatClient.prompt(prompt).call().chatResponse();
+        return outputConverter.convert(chatResponse.getResult().getOutput().getContent());
+
+    }
+
+
+    @GetMapping("/songs-pk")
+    public String getSongsByArtist(@RequestParam(value = "artist", defaultValue = "Kishor Kumar") String artist){
 
         var message  = """
                 Please give  me a list of top 10 songs for the artist {artist} . If you  don't know the answer, just say
